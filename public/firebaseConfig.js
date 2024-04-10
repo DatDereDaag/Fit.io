83b0172472d72c6b92f75afbfe80f73c7048c2a7
UnarmedChaitZ1 <158328318+UnarmedChaitZ1@users.noreply.github.com>
2024-04-10 17:45:14 -0400

    Add files via upload

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3A3e0MeItyKDJXWd0Lrw8_Ywd2Ui7iXs",
+    authDomain: "exerciseapp-6b2d3.firebaseapp.com",
+    projectId: "exerciseapp-6b2d3",
+    storageBucket: "exerciseapp-6b2d3.appspot.com",
+    messagingSenderId: "899332803103",
+    appId: "1:899332803103:web:c104c754fa9888b603232a",
+    measurementId: "G-7SLZW5C7GF"
+};
+
+export default firebaseConfig
